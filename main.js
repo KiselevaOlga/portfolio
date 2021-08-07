bf96f08ab8678c2a49fade0e0983c730b4fe8631
Olga Kiseleva <72406907+KiselevaOlga@users.noreply.github.com>
2021-08-07 15:15:51 +0100

    added overlay on projects div and animations

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log('Connected')
+console.log('Connected');
+
+//function which toggles animation in scroll
+const observer = new IntersectionObserver(entries => {
+    // Loop over the entries
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        }
+      // If the element is visible
+      if (entry.isIntersecting) {
+        // Add the animation class
+        entry.target.classList.add('animate__backInRight');
+        // entry.target.classList.add('animate__delay-1s');
+      }
+
+    });
+});
+const anEl = document.querySelectorAll(".animate__animated")
+anEl.forEach(el => {
+    observer.observe(el)
+})
